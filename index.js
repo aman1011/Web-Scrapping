966e31f31210187f3adcaf310c06fccbad77bd76
aveeralgaur <aveeral.firebolt@gmail.com>
2021-07-11 19:08:04 +0530

    add debugging statement

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const puppeteer = require('puppeteer');
         
         readline.close();
       });
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
+    console.log("printing city " + readline)
     await page.goto('https://apna.co/jobs/jobs-in-'+ readline);
     let data= await page.evaluate(() => {
         let headline = document.querySelector('h1[class="sc-17svb7l-2 dAKBvj"]').inner
@@ -19,6 +20,7 @@ const puppeteer = require('puppeteer');
             headline
         }
     });
+    console.log("Yo printing output")
     console.log(data);
     await browser.close();
 })();
