2f3cb463f09f65c5ce836c22c7121ca723ede8a8
Aman Gaur <gaman@athenahealth.com>
2021-07-20 12:28:28 +0530

    all jobs printed on  a single page

--- index.js
+++ index.js
@@ -1,26 +1,120 @@
+let cities = ['kanpur', 'lucknow', 'chandigarh', 'ahmedabad', 'bengaluru', 'chennai', 'delhi_ncr', 'hyderabad', 'jaipur', 'kolkata', 'ludhiana', 'mumbai', 'pune', 'ranchi','surat'];
+let categories = ["technician", "marketing", "human_resource"]
+//let cities = ['kanpur']
 
+console.log("\nThis program scrapes jobs from Apna.co")
+"use strict";
+
+
+/*Input */
+//const ps = require("prompt-sync");
+//const prompt = ps();
+//let city = prompt("You want job in which city? ").toLowerCase();
+//let category=prompt("What type of job do you want? Select a category(Technician/IT_Support/Marketing/Human_Resource/Teacher/Graphic_Designer/etc.): ").toLowerCase();
+
+
+/*Scrapping */
+const { exit } = require('process');
 const puppeteer = require('puppeteer');
-;(async() => {
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
+
+
+
+async function get_data() {
+  const browser = await puppeteer.launch();
+  const page =  await browser.newPage();
+
+  /*City */
+  for (var i = 0; i < cities.length; i++) {
+    for (var j = 0; j < categories.length; j++) {
+      city = cities[i];
+      category = categories[j];
+
+      console.log("Finding jobs for " + category + " in " + city);
+      page.goto('https://apna.co/jobs/jobs-in-' + cities[i]);
+      await page.waitForSelector('body');
+
+      const data = await page.evaluate(() => {
+        return document.querySelector('section[class="sc-17svb7l-0 iqnvTl"]>h1').innerText 
       });
-       
-      readline.question('Want job in which city? ', name => {
-        
-        readline.close();
+      console.log('\nThere are total', data)
+
+
+      page.goto('https://apna.co/jobs/'+category+'-jobs-in-' + city);
+      await page.waitForSelector('body');
+
+      const data1 = await page.evaluate(() => {
+        return document.querySelector('h1[class="sc-17svb7l-2 dAKBvj"]').innerHTML
       });
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
-    console.log("printing city " + readline)
-    await page.goto('https://apna.co/jobs/jobs-in-'+ readline);
-    let data= await page.evaluate(() => {
-        let headline = document.querySelector('h1[class="sc-17svb7l-2 dAKBvj"]').inner
-        return {
-            headline
-        }
-    });
-    console.log("Yo printing output")
-    console.log(data);
-    await browser.close();
-})();
+
+      console.log('There are total', data1)
+      //console.log('\n1 job which suits your choice is:')
+
+      /*job name */
+      // getting the all the nodes for the jobs.
+      // one thing to note here is the pagination.
+      // hit the url with pagination and check for the returned
+      // data. if undefined we have run out of the pages.
+      var jobNodes = await page.evaluate(() => {
+        let nodes = document.querySelectorAll('div[class="e8x9ni-0 jyAzZx"]>a');
+        let jobs = []
+        nodes.forEach(element => {
+          jobs.push(element.innerText)
+        })
+        //return document.querySelectorAll('div[class="e8x9ni-0 jyAzZx"]>a')[0].innerText
+        return jobs;
+      });
+
+      console.log("The numbr of jobs" + jobNodes.length);
+      jobNodes.forEach(element => {
+        //  splitting the job node on the basis of new-line character.
+        jobDetails = element.split("\n");
+        jobLookup = {};
+        jobLookup['Job Title'] = jobDetails[0];
+        jobLookup['Company'] = jobDetails[2];
+        jobLookup['Salary'] = jobDetails[4];
+        jobLookup['Experience'] = jobDetails[6];
+        jobLookup['Education'] = jobDetails[8];
+        console.log(jobLookup);
+      });
+      //console.log("Number of job nodes " + jobNodes[);
+      const data2 = await page.evaluate(() => {
+        return document.querySelector('p[class="sc-1y4fh76-4 fMBjOI"]').innerText
+      });
+
+      console.log('\nJob Name:',data2)
+
+      /*job company */
+      const data3 = await page.evaluate(() => {
+        return document.querySelector('p[class="sc-1y4fh76-5 bXUidx"]').innerText
+      });
+
+      console.log('Company Name:',data3)
+
+      /*salary */
+      const data4 = await page.evaluate(() => {
+        return document.querySelector('p[class="sc-1y4fh76-6 lksWMy"]').innerText
+      });
+
+      console.log('Salary Offered:',data4)
+    
+      /*experience */
+      const data5 = await page.evaluate(() => {
+        return document.querySelector('p[class="sc-1y4fh76-12 bspSEn"]').innerText
+      });
+
+      console.log('Experience Required:',data5)
+
+      /*address */
+      const data6 = await page.evaluate(() => {
+        return document.querySelector('p[class="sc-1y4fh76-17 gIfa-de"]').innerText
+      });
+
+    }
+  } 
+
+  await browser.close();
+  return ;
+}
+
+get_data()
+
